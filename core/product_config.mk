dbd3c57761b22e81b35c6ed776082e3a6df05692
Brint E. Kriebel <bekit@cyngn.com>
2014-10-14 12:43:28 -0700

    product_config: Set PA_BUILD priority above TARGET_BUILD_APPS
    Even if we are doing TARGET_BUILD_APPS, we may want to use a pa
    target for building.
    
    Change-Id: Ic8053bc679fa8b726be90e611aeaacdf1850cabc
    
    Conflicts:
    	core/product_config.mk

--- core/product_config.mk
+++ core/product_config.mk
@@ -179,19 +179,20 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-ifneq ($(strip $(TARGET_BUILD_APPS)),)
-# An unbundled app build needs only the core product makefiles.
-all_product_configs := $(call get-product-makefiles,\
-    $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
+# A PA build needs only the PA product makefiles.
+ifneq ($(PA_BUILD),)
+  all_product_configs := $(shell ls vendor/pa/products/pa_$(PA_BUILD).mk)
 else
-  ifneq ($(PA_BUILD),)
-    all_product_configs := $(shell ls device/*/$(PA_BUILD)/cm.mk)
+  ifneq ($(strip $(TARGET_BUILD_APPS)),)
+  # An unbundled app build needs only the core product makefiles.
+  all_product_configs := $(call get-product-makefiles,\
+      $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif
-endif
+  endif # TARGET_BUILD_APPS
+endif # PA_BUILD
 
 ifeq ($(PA_BUILD),)
 # Find the product config makefile for the current product.
